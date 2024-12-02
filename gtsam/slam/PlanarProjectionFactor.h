1395b9fae74bb85fc8a84a388d3b76ee9f616718
Joel Truher <joel@truher.org>
2024-12-02 11:35:05 -0800

    new factors

--- gtsam/slam/PlanarProjectionFactor.h
+++ gtsam/slam/PlanarProjectionFactor.h
@@ -11,3 +11,16 @@
  * @date Dec 2, 2024
  * @author joel@truher.org
  */
+#pragma once
+#include <gtsam/nonlinear/NonlinearFactor.h>
+namespace gtsam {
+    class PlanarProjectionFactor : public NoiseModelFactor {
+    public:
+        PlanarProjectionFactor() {}
+        ~PlanarProjectionFactor() override {}
+        Vector evaluateError(const Pose3& pose, const Point3& point,
+            OptionalMatrixType H1, OptionalMatrixType H2) const override {
+        }
+
+    };
+} // namespace gtsam
--- gtsam/slam/PlanarSFMFactor.h
+++ gtsam/slam/PlanarSFMFactor.h
@@ -6,9 +6,21 @@
  * * constant landmarks
  * * batched input
  * * numeric differentiation
- * 
+ *
  * @file PlanarSFMFactor.h
  * @brief for planar smoothing with unknown calibration
  * @date Dec 2, 2024
  * @author joel@truher.org
  */
+#pragma once
+#include <gtsam/nonlinear/NonlinearFactor.h>
+namespace gtsam {
+    class PlanarSFMFactor : public NoiseModelFactor {
+    public:
+        PlanarSFMFactor() {}
+        ~PlanarSFMFactor() override {}
+        Vector evaluateError(const Pose3& pose, const Point3& point,
+            OptionalMatrixType H1, OptionalMatrixType H2) const override {
+
+        };
+    } // namespace gtsam
