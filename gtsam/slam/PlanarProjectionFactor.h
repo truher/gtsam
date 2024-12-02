61665c2fa4cf9db03cf6a1461d9b654d481988da
Joel Truher <joel@truher.org>
2024-12-02 10:40:51 -0800

    new factors

---
+++ gtsam/slam/PlanarProjectionFactor.h
@@ -0,0 +1,13 @@
+/**
+ * Similar to GenericProjectionFactor, but:
+ *
+ * * 2d pose variable (robot on the floor)
+ * * constant landmarks
+ * * batched input
+ * * numeric differentiation
+ *
+ * @file PlanarSFMFactor.h
+ * @brief for planar smoothing
+ * @date Dec 2, 2024
+ * @author joel@truher.org
+ */
---
+++ gtsam/slam/PlanarSFMFactor.h
@@ -0,0 +1,14 @@
+/**
+ * Similar to GeneralSFMFactor, but:
+ *
+ * * 2d pose variable (robot on the floor)
+ * * camera offset variable
+ * * constant landmarks
+ * * batched input
+ * * numeric differentiation
+ * 
+ * @file PlanarSFMFactor.h
+ * @brief for planar smoothing with unknown calibration
+ * @date Dec 2, 2024
+ * @author joel@truher.org
+ */
